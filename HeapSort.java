413aa25cc68f9eec2726ed9a13ebb7e5fa40507c
Kotikov <11deadsluts@gmail.com>
2023-06-25 14:11:04 +0300

    heapify is over

--- HeapSort.java
+++ HeapSort.java
@@ -1,14 +1,36 @@
 public class HeapSort {
     public static void main(String[] args) {
-        
+
     }
+
     public static void heapSort(int[] arr) {
-        for (int i = arr.length/2-1; i >=0; i--) {
-        } 
-        for (int i = arr.length-1; i > 0 ; i--) {
-            int temp = arr[0];
-            arr[0] = arr [i];
-            arr[i] = temp;
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            heapify(arr, arr.length, i);
         }
-    }        
+        for (int i = arr.length - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    static void heapify(int[] arr, int size, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < size && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < size && arr[right] > arr[largest]) {
+            largest = right;
+        }
+        if (largest !=i) {
+            int swap = arr[i];
+            arr [i] = arr[largest];
+            arr[largest] = swap;
+        }
+        heapify(arr, size, largest);
+    }
 }
