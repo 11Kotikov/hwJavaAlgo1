552dc9ce1ccd6b9e1956ab5ad9331e1115579c78
Kotikov <11deadsluts@gmail.com>
2023-06-25 13:55:24 +0300

    heap sort over/heapify start

--- HeapSort.java
+++ HeapSort.java
@@ -2,7 +2,13 @@ public class HeapSort {
     public static void main(String[] args) {
         
     }
-
-
-        
+    public static void heapSort(int[] arr) {
+        for (int i = arr.length/2-1; i >=0; i--) {
+        } 
+        for (int i = arr.length-1; i > 0 ; i--) {
+            int temp = arr[0];
+            arr[0] = arr [i];
+            arr[i] = temp;
+        }
+    }        
 }
